37643c9f2b457ed20f7eda0e9dd037c143a88394
Tijani Fulani <Fulani07@gmail.com>
2021-07-17 10:08:44 +0100

    created acrt item and cart files.

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button'
+import React from 'react'
+
+// Types
+
+import { CartItemType } from '../App'
+
+// styles
+import { Wrapper } from './CartItem.styles'
+
+ const CartItem: React.FC =() => <div>Cart</div>
+
+ export default CartItem
